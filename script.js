b5c034af7da438aa100a0ed51ba5b4d7c6fe2fb5
wmcheng <cwm96107@gmail.com>
2022-01-09 16:25:30 +0800

    add build new grid

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 // let gridSize = prompt("Enter grid size here");
 let gridSize = 16;
 let boxSize = 2;
@@ -14,13 +13,27 @@ gridContainer = document.querySelector(".grid-container");
 //     container.appendChild(box);
 // }
 
-gridContainer.style = `grid-template-columns: repeat(${gridSize}, minmax(0, 1fr))`;
-// gridContainer.style = `grid-auto-rows: 1fr`;
-
-for(let i = 0; i < (gridSize * gridSize); i++) {
-    box = document.createElement("div");
-    // box.textContent = i;
-    box.classList.add('box');
-    // box.style.padding = `${boxSize * 0.5}em`;
-    gridContainer.appendChild(box);
+function buildGrid(gridSize) {
+    gridContainer.style = `grid-template-columns: repeat(${gridSize}, minmax(0, 1fr))`;
+    for(let i = 0; i < (gridSize * gridSize); i++) {
+        box = document.createElement("div");
+        // box.textContent = i;
+        box.classList.add('box');
+        // box.style.padding = `${boxSize * 0.5}em`;
+        gridContainer.appendChild(box);
+    }
 }
+
+buildGrid(gridSize);
+
+btn = document.querySelector(".btn");
+btn.addEventListener('click', () => {
+    while(gridContainer.hasChildNodes()) {
+        console.log("remove child");
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+    do {
+        gridSize = prompt("Enter new grid size here:");
+    } while (gridSize > 100);
+    buildGrid(gridSize);
+});
